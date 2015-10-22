9323829fb7ea4970071de356e070c2977fd335c8
YutongZhang <yutongzhang.tony@gmail.com>
2015-10-22 06:13:57 +0000

    Main Page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
+  
+  get 'home' =>'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions"}
   devise_scope :user do
     root "users/sessions#new"
