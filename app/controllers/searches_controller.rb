d9d22605eb5ed2244267eeb7f6f48ac2ad8e40f2
renqing <renqing@1992y.com>
2015-11-05 16:28:35 -0800

    required login before search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,16 @@
 class SearchesController < ApplicationController
+  before_action  :check_access
+
     #search action
   def search
     @restaurants = Restaurant.search(params[:search])
     @foods = Food.search(params[:search])
+  end
+
+  private
+  def check_access
+  	if current_user.nil?
+  		redirect_to '/' and return
+  	end
   end
 end
