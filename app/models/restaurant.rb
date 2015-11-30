59bfbb4cddd350af0a4a8d7242bc524fe5c54598
renqing <renqing@1992y.com>
2015-11-29 16:17:28 -0800

    optimize rating calc

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
   belongs_to :user
-  has_many :foods
-  has_many :comments
+  #has_many :foods
+  #has_many :comments
   
   geocoded_by :address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
